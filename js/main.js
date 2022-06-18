df1c95006f61b8d55895a773bacb3e3e7b649b8c
kimjin2 <103062714+kimjin2@users.noreply.github.com>
2022-06-19 01:55:27 +0900

    dcdc
    cdcdcd

--- js/main.js
+++ js/main.js
@@ -6,10 +6,10 @@ $(function(){
 	var menu = $(".gnb > li");
 	var content = $("section");//section 으로 변경할 부분
 	
-	menu.click(function(){
+	menu.click(function(event){
 		/*preventDefault 는 a 태그 처럼 클릭 이벤트 외에 
 별도의 브라우저 행동을 막기 위해 사용됩니다.*/
-		
+		event.preventDefault();
 		
 		//사용자가 클릭한 li
 		var tg = $(this);
@@ -20,30 +20,22 @@ $(function(){
 		//선택된 영역의 top 의 좌표값을 저장
 		//.offset()은 선택한 요소의 좌표를 가져오거나 특정 좌표로 이동하게 합니다.
 		var tt = section.offset().top;
-		$('.gnb > li').each(function(){
-	
-			$(this).click(function(){
-			   $('.gnb > li').removeClass('active');
-			   $(this).addClass('active')
-			});
-			
-		 });
  
 		//스크롤이 tt의 값에 맞게 움직이게
 	$("html,body").stop().animate({scrollTop:tt});
 		});//menu.click() 끝
 		
 		// 윈도우에서 scroll() 스크롤이 작동될 때 일어날 일.
-		$(".wrap").scroll(function(){
+		$(window).scroll(function(){
 		//.scrollTop()은 선택한 요소의 스크롤바 수직 위치를 반환하거나 스크롤바 수직 위치를 정합니다.
-		var location = $(".wrap").scrollTop();
+		var location = $(window).scrollTop();
 		
 		content.each(function() {
 			//반복문(each)
       var tg = $(this);
 			var idx = tg.index();
 			
-			if(tg.offset().top - 50 <= location){  //active 위치가 안맞으면 location + 위치값 을 추가하면 됨
+			if(tg.offset().top <= location){  //active 위치가 안맞으면 location + 위치값 을 추가하면 됨
 				menu.removeClass("active");
 				menu.eq(idx).addClass("active");
 				}
@@ -94,7 +86,14 @@ $(function(){
 
    });//모바일 네비 fade
 
-   
+   $('.gnb > li').each(function(){
+	
+	 $(this).click(function(){
+		$('.gnb > li').removeClass('active');
+		$(this).addClass('active')
+	 });
+	 
+  });
 
 
     $('.wrap').scroll(function(){
